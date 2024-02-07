8443e6bc75213bc9fb49b4c209e9733cb1b78b25
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-02-07 22:20:37 +0545

    Added basic layout of the website

---
+++ src/Pages/Homescreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopIntroSection from '../TopIntroSection';
+import Footer from '../../components/Footer';
+
+const Home = () => (
+  <>
+    <TopIntroSection />
+    <Footer />
+  </>
+);
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-scroll';
+
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(false);
+
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+
+  const closeMenu = () => {
+    setNavActive(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 500) {
+        closeMenu();
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (window.innerWidth <= 1200) {
+      closeMenu();
+    }
+  }, []);
+
+  return (
+    <nav className={`navbar ${navActive ? 'active' : ''}`}>
+      <div>
+        <h3>Pushkar.dev</h3>
+      </div>
+      <button
+        type="button"
+        className={`nav__hamburger ${navActive ? 'active' : ''}`}
+        onClick={toggleNav}
+        aria-label="Toggle Menu"
+      >
+        <span className="nav__hamburger__line" />
+        <span className="nav__hamburger__line" />
+        <span className="nav__hamburger__line" />
+      </button>
+      <div className={`navbar--items ${navActive ? 'active' : ''}`}>
+        <ul>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy
+              smooth
+              offset={-70}
+              duration={500}
+              to="heroSection"
+              className="navbar--content"
+              role="menuitem"
+              tabIndex={0}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy
+              smooth
+              offset={-70}
+              duration={500}
+              to="MyPortfolio"
+              className="navbar--content"
+              role="menuitem"
+              tabIndex={0}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy
+              smooth
+              offset={-70}
+              duration={500}
+              to="AboutMe"
+              className="navbar--content"
+              role="menuitem"
+              tabIndex={0}
+            >
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy
+              smooth
+              offset={-70}
+              duration={500}
+              to="Contact"
+              className="navbar--content"
+              role="menuitem"
+              tabIndex={0}
+            >
+              Contact
+            </Link>
+
+          </li>
+          <Link
+            onClick={closeMenu}
+            activeClass="navbar--active-content"
+            spy
+            smooth
+            offset={-70}
+            duration={500}
+            to="testimonial"
+            className="navbar--content"
+            role="menuitem"
+            tabIndex={0}
+          >
+            <div className="flag-cont">
+              <img className="flag" src="./img/flag.svg" alt="Nepal" />
+              Nepal
+            </div>
+          </Link>
+        </ul>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
